63606569f63ae42aed73c209b890892a5ee581e9
Felicity Ng <doodlefelicity@gmail.com>
2018-12-10 15:03:27 -0500

    fixed svg files

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -24,6 +24,7 @@ function changeNavImage(e){
   image.src = navImages[e.target.textContent];
   navDiv.appendChild(image);
   e.target.addEventListener('mouseleave',removeNavImage);
+  e.target.addEventListener('focusout',removeNavImage);
 }
 
 function redirect(e){
@@ -34,5 +35,6 @@ window.addEventListener('load', () => {
   for(let i=0; i<5; i++){
     let nav = document.querySelector('#nav'+i);
     nav.addEventListener('mouseover',changeNavImage);
+    nav.addEventListener('focus',changeNavImage);
   }
 });
