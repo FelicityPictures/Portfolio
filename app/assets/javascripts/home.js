a280d2bd32db56ec197167ebfa55b272a04b0f92
Felicity Ng <doodlefelicity@gmail.com>
2019-01-30 20:45:03 -0500

    added click redirect to home

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,40 +1,9 @@
-const navImages = {
-  "About" : 'icons/about.png',
-  "UX/UI Work" : 'icons/ux.png',
-  "Graphic Design" : 'icons/graphic_design.png',
-  "CS Projects" : 'icons/cs.png',
-  "Resume" : 'icons/resume.png'
-};
-const idToLink = {
-  "nav0" : "about",
-  "nav1" : "ux",
-  "nav2" : "graphicdesign",
-  "nav3" : "cs",
-  "nav4" : "resume"
-}
-function removeNavImage(e){
-  const navDiv = document.querySelector('#nav-image');
-  navDiv.innerHTML = '';
-}
+let aboutButton = document.querySelector("#aboutButton");
+const projectsButton = document.querySelector("#projectsButton");
 
-function changeNavImage(e){
-  const navDiv = document.querySelector('#nav-image');
-  navDiv.innerHTML = "";
-  const image = document.createElement('img');
-  image.src = navImages[e.target.textContent];
-  navDiv.appendChild(image);
-  e.target.addEventListener('mouseleave',removeNavImage);
-  e.target.addEventListener('focusout',removeNavImage);
-}
-
-function redirect(e){
-  window.location.href = "./" + idToLink[e.target.id] + ".html";
-}
-
-window.addEventListener('load', () => {
-  for(let i=0; i<5; i++){
-    let nav = document.querySelector('#nav'+i);
-    nav.addEventListener('mouseover',changeNavImage);
-    nav.addEventListener('focus',changeNavImage);
-  }
+aboutButton.addEventListener("click", function(){
+  window.location.href = "/about";
+});
+projectsButton.addEventListener("click", function(){
+  window.location.href = "/projects";
 });
